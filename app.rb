41fbe3ce8878554f9f71378ad393992c944e32b3
Corey Garcia <corey.w.garcia@outlook.com>
2018-07-31 14:30:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Hello class, super excited to teach Siantra today"
+    "Hello class, super excited to teach Siantra today. Are you ready to learn in learn?"
   end
 
 end
